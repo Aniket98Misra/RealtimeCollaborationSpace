9cc32bd2c0265382955f744ad08ece0e1292e670
Aniket98Misra <misra98aniket@gmail.com>
2024-10-27 23:49:54 +0530

    adding realtime cursor

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -61,3 +61,13 @@ export const updateDocument = async (roomId: string, title: string) => {
     console.log(`Error while updating a room: ${error}`);
   }
 };
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error while getting rooms: ${error}`);
+  }
+};
